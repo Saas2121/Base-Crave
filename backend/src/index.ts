40f8c652badf66e9c60bedd8bc8e1e40869846c8
Sergio Arias <sergio.arias8@hotmail.com>
2026-05-30 17:17:54 -0500

    feat(backend): export app and prevent listen on Vercel deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -36,6 +36,10 @@ app.use(logErrors);
 app.use(wrapErrors);
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production' && !process.env.VERCEL) {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+export default app;
